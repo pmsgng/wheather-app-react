e1583e417789a4d000fb53c8a764787d0adacfea
Vlad <pmsgng88@gmail.com>
2022-03-27 19:32:06 +0300

    weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import WeatherBox from './WeatherBox';
+import ErrorWeather from './ErrorWeather';
+
+const api = {
+  key: '83c53addba9a911adc72132a8321e587',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
+  const [query,setQuery] = useState('');
+  const [weather,setWeather] = useState({})
+
+  const search = (e) => {
+    if(e.key === 'Enter')
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(res => res.json())
+    .then(result => {
+      setWeather(result)
+      setQuery('')
+      console.log(result)
+    })
+    
+  }
+
+
+  const dateBuilder = (d) => {
+    let months = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'];
+    let days = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const renderWeather = () => {
+
+    if(typeof weather.main !== 'undefined') {
+      return <WeatherBox dateBuilder={dateBuilder} weather={weather} />
+    } else if(weather.cod === '404') {
+      return <ErrorWeather />
+    } else {
+      return ''
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 5) ? 'app warm' : 'app'): 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            placeholder='Найти...'
+            className='search-bar'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyDown={search}
+          />
+        </div>
+        {renderWeather()}
+      </main>
     </div>
   );
 }
---
+++ src/ErrorWeather.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorWeather = () => {
+    return (
+        <div className='error'>
+            <h3>Ошибка... Попробуйте снова!</h3>
+        </div>
+    );
+};
+
+export default ErrorWeather;
